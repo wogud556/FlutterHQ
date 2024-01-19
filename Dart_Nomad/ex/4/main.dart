cab7433d32a4d2adf00d5ac707fb83700dabc8d7
wogud556 <wogud556@naver.com>
2024-01-19 10:42:31 +0900

    20240119

--- Dart_Nomad/ex/4/main.dart
+++ Dart_Nomad/ex/4/main.dart
@@ -1,7 +1,7 @@
 void main() {
   String? nico = 'nico'; //? 키워드로 null 이 들어올 수 있음을 미리 알려줌 코틀린이랑 비슷하다.
   nico = null;
-  if(nico != null) {
+  if(nico != null) { // 이때 dart는 nico가 확실히 null 이라는 사실을 알게됨
     nico.isNotEmpty;
   }
 }
