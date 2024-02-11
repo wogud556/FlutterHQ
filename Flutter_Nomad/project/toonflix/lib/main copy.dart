068aad9ab36f829671bcd9c6ce219ffe70edbe4f
wogud556 <wogud556@naver.com>
2024-02-11 17:22:35 +0900

    20240211_2

---
+++ Flutter_Nomad/project/toonflix/lib/main copy.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/button.dart';
+import 'package:toonflix/widgets/card.dart';
+
+const taxAmount = 15;
+const priceAmount = 30;
+
+var finalPrice = taxAmount + priceAmount;
+
+class Player {
+  String? name; //갖거나 가지지 않을 수도 있다.
+
+  Player({required this.name});
+}
+
+void main() {
+  var nico = Player(name: "potato");
+  //runApp은 리턴이 없는 함수
+  //위젯을 합치는 방식으로 화면을 만들어 내는 방식
+  //앱 생성의 시작부분이다.
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  //statelessWidget은 widget을 상속받으므로 build를 구현해야함
+  int counter = 0;
+
+  void onClicked() {
+    counter = counter + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //머터리얼이나 쿠퍼티노를 리턴해줌
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
